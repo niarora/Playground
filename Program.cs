b1305de6a460d64fdb25e5cf1ab6b48d36fef802
Nikhil Arora <nikhilarora@outlook.com>
2016-05-31 00:49:41 -0700

    First tested iteration of Max Contiguous Subsequence. This is not really Dynamic Programming. It is more of a greedy approach that keeps track of the current maximum, current total, and maximum of all maximums at any given time.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-using System;
-
-namespace Playground 
+namespace Playground
 {
-	public static class Program 
-	{
-		public static void Main() 
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    using System;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var input = new[] { 10, 2, -1, -2, 0, 3, -1, -10, 11, 5 };
+            var result = DynamicProgramming.MaxContiguousSubSequence.FindSequence(input);
+            for (var i = result.Start; i <= result.End; i++)
+			{
+				Console.Write("{0}, ", input[i]);
+			}
+			
+			Console.WriteLine();
+        }
+    }
 }
