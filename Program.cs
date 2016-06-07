f9e3c9f8d39d1f93a7c33889389fd74ac5d13baa
Nikhil Arora <nikhilarora@outlook.com>
2016-06-06 22:39:22 -0700

    Partition a linked list around Pivot element such that all elements smaller than Pivot are to the left of Pivot. Pivot itself may or may not be in the list. This implementation runs in O(n). n = |List|.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 namespace Playground
 {
+    using System;
+    using System.Collections.Generic;
+
     using Playground.Extensions;
 
     public static class Program
     {
         public static void Main()
         {
-            var s = "nice";
-            var permutations = Playground.Utils.Permutation.Permutations(s);
-            permutations.WriteLine();
+            var list = new LinkedList<int>(new[] { 3, 10, 1, 5, 6, 5, 4 });
+            list.Partition(5);
+            list.WriteLine();
+            list = new LinkedList<int>(new int[] { 1 });
+            list.Partition(5);
+            list.WriteLine();
+            list = new LinkedList<int>(new int[] { 10, 5, 4 });
+            list.Partition(5);
+            list.WriteLine();
         }
     }
 }
