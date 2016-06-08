8cfc75efa487700dfbfe36f85e73da67c28f5a9a
Nikhil Arora <nikhilarora@outlook.com>
2016-06-07 21:48:18 -0700

    Program to compress a string. E.g. nniii is encoded as n2i3. nnii is encoded as n2i2, which is the same length as the original so nnii is returned back.

--- Extensions/StringExtension.cs
+++ Extensions/StringExtension.cs
@@ -2,17 +2,51 @@ namespace Playground.Extensions
 {
     using System;
     using System.Collections.Generic;
+    using System.Text;
 
     public static class StringExtension
     {
         public static void WriteLine(this IEnumerable<string> strings)
         {
-            foreach(var s in strings)
+            foreach (var s in strings)
             {
                 Console.WriteLine(s);
-            }			
-			
+            }
+
             Console.WriteLine();
+        }
+
+        public static string Compress(this string s)
+        {
+            var length = 0;
+            if (s == null || (length = s.Length) <= 1)
+            {
+                return s;
+            }
+
+            var builder = new StringBuilder();
+            builder.Append(s[0]);
+            var occurence = 1;
+            var i = 1;
+
+            //// Break if builder will be at least length of 's - 1' in this loop.
+            for (; builder.Length < length - 1 && i < length; i++)
+            {
+                if (s[i] == s[i - 1])
+                {
+                    occurence++;
+                }
+                else
+                {
+                    builder.Append(occurence);
+                    builder.Append(s[i]);
+                    occurence = 1;
+                }
+            }
+
+            builder.Append(occurence);
+
+            return builder.Length < length ? builder.ToString() : s;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 namespace Playground
 {
+    using System;
     using Playground.Extensions;
 
     public static class Program
     {
         public static void Main()
         {
-            var s = "nice";
-            var permutations = Playground.Utils.Permutation.Permutations(s);
-            permutations.WriteLine();
+            var s = "nniiccc";
+            var compressed = s.Compress();
+            Console.WriteLine("Original: {0}, Compressed: {1}", s, compressed);
+            Console.WriteLine("Original Length: {0}, Compressed Length: {1}", s.Length, compressed.Length);
         }
     }
 }
