b92fc76b6ec650aa1df62fb1d70c2442d4676007
Nikhil Arora <nikhilarora@outlook.com>
2016-06-07 22:35:07 -0700

    Compress a string. E.g. nniiiccc -> n2i3c3 nnii -> nnii String is only compressed if the compressed string is shorter than the original.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 namespace Playground
 {
     using System;
-    using System.Collections.Generic;
 
     using Playground.Extensions;
 
@@ -9,15 +8,7 @@ namespace Playground
     {
         public static void Main()
         {
-            var list = new LinkedList<int>(new[] { 3, 10, 1, 5, 6, 5, 4 });
-            list.Partition(5);
-            list.WriteLine();
-            list = new LinkedList<int>(new int[] { 1 });
-            list.Partition(5);
-            list.WriteLine();
-            list = new LinkedList<int>(new int[] { 10, 5, 4 });
-            list.Partition(5);
-            list.WriteLine();
+            Console.WriteLine("nniiiccc".Compress());
         }
     }
 }
