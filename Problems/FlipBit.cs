34cf6716bb622c66a0ba7c45d2c34853b8a0a8c5
Nikhil Arora <nikhilarora@outlook.com>
2016-06-21 23:20:18 -0700

    Given a positive integer, calculate the next largest and smallest number with same number of bits set to  '1'. E.g. 9, i.e. 1001, returns: Largest: 1010, Smallest: 0011.

--- Problems/FlipBit.cs
+++ Problems/FlipBit.cs
@@ -8,7 +8,7 @@ namespace Playground.Problems
         {
             return FlipBit.GetLongestSequenceOfOnesWithSingleFlip((uint)n);
         }
-        
+
         // If we try to right shift a signed integer the high order empty bits are signed to the sign bit.
         // i.e. -1 >> 1 == -1. Thus it is better to cast to uint before right shifting, if the desired behaviour
         // is to fill the high order bits with zeroes.
@@ -41,6 +41,95 @@ namespace Playground.Problems
             }
 
             return max1s;
+        }
+
+        // Given a positive integer, calculate the next largest and smallest number with same 
+        // number of bits set to  '1'. E.g. 9, i.e. 1001, returns:
+        // Largest: 1010, Smallest: 0011. 
+        public static Tuple<int, int> NextSmallestAndLargestWithSameOneBits(int n)
+        {
+            if (n < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(n), "The value must be greater than or equal to 1.");
+            }
+
+            int largest = 0, smallest = 0;
+            if (n == 1) // Special case where the smallest value with a 
+            {            // single bit as 1 is int.MinValue.
+                largest = 2;
+                smallest = int.MinValue;
+            }
+            else
+            {
+                var m = n;
+                var firstOneIndex = GetIndexOfNextBitFromRight(n, 0, true);
+                if (firstOneIndex == 30)
+                {
+                    throw new InvalidOperationException(
+                        string.Format(
+                            "No number larger than {0} exists with only one bit set to '1'.",
+                            n));
+                }
+
+                // Set first '1' bit from right to '0'.                
+                m &= (~(1 << firstOneIndex));
+                var nextZeroIndex = GetIndexOfNextBitFromRight(n, firstOneIndex + 1, false);
+                // Set the zero after the first '1' bit from the right to 1.
+                m |= (1 << nextZeroIndex);
+                largest = m;
+
+                m = n;
+                var firstZeroIndex = GetIndexOfNextBitFromRight(n, 0, false);
+                // Set the first '0' bit to '1'.
+                m |= 1 << firstZeroIndex;
+                var nextOneIndex = GetIndexOfNextBitFromRight(n, firstZeroIndex + 1, true);
+                // Set the next '1' bit to '0'.
+                m &= (~(1 << nextOneIndex));
+                smallest = m;
+            }
+
+            return Tuple.Create(largest, smallest);
+        }
+
+        // Throws an exception if the bit can't be find in position 0..30.
+        // The 31st index is the position of the signed bit.  
+        private static int GetIndexOfNextBitFromRight(int num, int start, bool findOne)
+        {
+            var i = start;
+            if (findOne)
+            {
+                for (; i < 31; i++)
+                {
+                    var leftShiftedMask = 1 << i;
+                    if ((leftShiftedMask & num) > 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (; i < 31; i++)
+                {
+                    var leftShiftedMask = 1 << i;
+                    if ((leftShiftedMask & num) == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (i == 31)
+            {
+                throw new InvalidOperationException(
+                    string.Format(
+                        "Couldn't find bit: {0}, in: {1}, starting at position {2}.",
+                        Convert.ToInt32(findOne),
+                        num,
+                        start));
+            }
+
+            return i;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,20 @@ namespace Playground
         public static void Main()
         {
             var binaryString = "111101111010010110111";
-            var n = Convert.ToInt32(binaryString, 2);
-            var result = FlipBit.GetLongestSequenceOfOnesWithSingleFlip(n);
+            bool loop = true;
+            while (loop)
+            {
+                var n = Convert.ToInt32(binaryString, 2);
+                var result = FlipBit.NextSmallestAndLargestWithSameOneBits(n);
+                var nextLargest = result.Item1;
+                var nextSmallest = result.Item2;
+                Console.WriteLine(n);
+                Console.WriteLine(Convert.ToString(n, 2));
+                Console.WriteLine(nextLargest);
+                Console.WriteLine(Convert.ToString(nextLargest, 2));
+                Console.WriteLine(nextSmallest);
+                Console.WriteLine(Convert.ToString(nextSmallest, 2));
+            }
         }
     }
 }
