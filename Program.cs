8a78e0ca2423d9219e56752f09c16c7111b554ba
Nikhil Arora <nikhilarora@outlook.com>
2016-08-14 20:15:28 -0700

    Compute Mnemonics list for a given number.

--- Program.cs
+++ Program.cs
@@ -1,32 +1,26 @@
 namespace Playground
 {
     using System;
-    using System.Collections.Generic;
+    using System.Linq;
     using Playground.Extensions;
     public static class Program
     {
         public static void Main()
         {
-            Test(new int[] { 1, 0, 3, 4, 5, 7, 8 }, new int[] { 9, 9 });
-            Test(new int[] { }, new int[] { 1 });
-            Test(new int[] { 9, 9 }, new int[] { 9, 9, 9, 9, 9 });
-            Test(new int[] { 9, 9, 9, 9, 9 }, new int[] { 9, 9 });
-            Test(new int[] { }, new int[] { });
+            Test("2");
+            Test("97");
+            Test("3236986237");
         }
 
-        private static void Test(IEnumerable<int> op1, IEnumerable<int> op2)
+        private static void Test(string digits)
         {
-            var op1List = new LinkedList<int>(op1);
-            var op2List = new LinkedList<int>(op2);
-            Console.WriteLine();
-            Console.WriteLine("Operand 1: ");
-            op1List.Print();
-            Console.WriteLine("Operand 2: ");
-            op2List.Print();
-            var sum = op1List.SumLists(op2List);
-            Console.WriteLine("Sum of both operands: ");
-            sum.Print();
-            Console.WriteLine();
+            var mnemonics = new Problems.Mnemonics();
+            var result = mnemonics.ComputeMnemonics(digits);
+            Console.WriteLine("Input digits:");
+            digits.Print();
+            Console.WriteLine("All Mnemonics: ");
+            result.Print();
+            Console.WriteLine("Total Mnemonics: {0}", result.Count());
         }
     }
 }
